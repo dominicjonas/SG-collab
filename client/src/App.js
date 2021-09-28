43389684134521f8ebed7dad12fb4c753a77a59c
Dominic Fergal <dominicfergal@Dominics-MacBook-Air.local>
2021-09-28 11:57:47 +0100

    Added footer placeholder

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Home from './components/common/Home.js'
 import Navbar from './components/common/Navbar.js'
 
 import Brands from './components/brandsAndGuitars/Brands.js'
+import Footer from './components/common/Footer.js'
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route exact path='/brands' component={Brands} />
         <Route exact path='/' component={Home} />
       </Switch>
+      <Footer />
     </BrowserRouter>
   )
 }
